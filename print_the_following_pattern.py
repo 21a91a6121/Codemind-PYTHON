edef45802fd1cd64eb34883adca7731ae7a42ce1
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-04-30 10:27:30 +0530

    n = int(input()) for row in range(n+1,1,-1):     for col in range(1,row):         print(col,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
 n = int(input())
-for row in range(1,n+1):
-    for col in range(n,0,-1):
-        print(col,end=" ")
+for row in range(n+1,1,-1):
+    for col in range(1,row):
+        print(col,end="")
     print()
+        
