a4b43c86cee90ded614afd166ececf06cefcd35c
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-05-19 11:24:09 +0530

    a, b = map(int, input().split()) while a!=b:     if a>b:         a=a-b     elif a<b:         b=b-a print(a)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,11 +1,7 @@
-a,b = map(int,input().split())
-if a>b:
-    small=b
-else:
-    small=a
-
-for i in range(1, small+1):
-    if(a%i==0 and b%i==0):
-        gcd=i
-
-print(gcd)
+a, b = map(int, input().split())
+while a!=b:
+    if a>b:
+        a=a-b
+    elif a<b:
+        b=b-a
+print(a)
