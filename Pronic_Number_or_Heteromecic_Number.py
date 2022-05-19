ea77f1f5f0f8d0bf2538d990e558aa5eda161a1d
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-05-19 11:19:11 +0530

    num = int(input()) c=0 for i in range(1,num,1):     sum = i*(i+1)     if(num==sum):         c=c+1 if c==1:     print("YES") else:     print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,10 +1,11 @@
-n = int(input())
-flag = 0
-for i in range(1,n+1):
-    if(i*(i+1)==n):
-        flag = flag+1
-if flag>0:
+num = int(input())
+c=0
+for i in range(1,num,1):
+    sum = i*(i+1)
+    if(num==sum):
+        c=c+1
+if c==1:
     print("YES")
 else:
     print("NO")
-     
+        
