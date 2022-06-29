32c01a53552335747f616642118c7c2e4e737e19
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-29 10:33:34 +0530

    def addDigits(num):
        while(num>=10):
            temp=0
            while(num>0):
                temp=temp+num%10
                num=num//10
                
            num=temp
            
        return num
    num=int(input())
    print(addDigits(num))

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,15 +1,13 @@
-def add_digits(n):
-    while n>=10:
-        temp = 0
-        while n>0:
-            temp = temp+n%10
-            n = n//10
-        n = temp
-    return n
-num = int(input())
-if (add_digits(num)>=10):
-    add_digits(num)
-else:
-    print(add_digits(num))
+def addDigits(num):
     
+    while(num>=10):
+        temp=0
+        while(num>0):
+            temp=temp+num%10
+            num=num//10
             
+        num=temp
+        
+    return num
+num=int(input())
+print(addDigits(num))
