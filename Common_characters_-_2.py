81a9bde438e6756df88a1180a4fa0af0d5518863
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-30 21:31:44 +0530

    a=input() a2="" res="" for i in a:     if i.isupper():         a2+=i.lower()     else:         a2+=i arr=list(a2.split()) for i in arr:     for j in i:         f=0         for k in arr:             if j in k:                 f=1             else:                 f=0                 break         if f==1:             res+=j     break if len(res)==0:     print(0) else:     print(len(res))

--- Common_characters_-_2.py
+++ Common_characters_-_2.py
@@ -1,13 +1,25 @@
-s=input().lower()
-l=s.split()
-l1=list(l[0])
-l.remove(l[0])
-c=0
-e=[]
-for i in l1:
-    for j in range(len(l)):
-        if i not in l[j] and i not in e:
-            break
+a=input()
+a2=""
+res=""
+for i in a:
+    if i.isupper():
+        a2+=i.lower()
     else:
-        e.append(i)
-print(len(e))
+        a2+=i
+arr=list(a2.split())
+for i in arr:
+    for j in i:
+        f=0
+        for k in arr:
+            if j in k:
+                f=1
+            else:
+                f=0
+                break
+        if f==1:
+            res+=j
+    break
+if len(res)==0:
+    print(0)
+else:
+    print(len(res))
